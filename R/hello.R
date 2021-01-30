0cda24efbcb43cee6d24235703d9817df3c6e699
muskuloes <t.victor@campus.lmu.de>
2021-01-30 01:40:45 +0100

    export functions and provide examples

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,9 @@
 #' Print Hello World!
 #'
 #' @return "Hello World!"
+#' @export
+#' @examples
+#' hello_world()
 hello_world <- function() {
   return("Hello World!")
 }
@@ -9,7 +12,10 @@ hello_world <- function() {
 #'
 #' @param name string name to say hello to
 #' @return Hello (name)
+#' @export
 #' @import checkmate
+#' @examples
+#' hello("James")
 hello <- function(name) {
   checkmate::assert_character(name, len = 1)
   return(sprintf("Hello %s!", name))
